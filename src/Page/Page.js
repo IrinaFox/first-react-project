81edd1ca6c4c01ac0bfca56cc6f2bff1c07f7910
IrinaFox <irina.fox88@gmail.com>
2017-05-05 03:06:14 +0300

    delete class and added const for export

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import './Page.css';
 
-class Page extends Component {
-	render() {
-		return (
-			<div>
-				<ul>
-					<li>
-						<a>Main</a>
-					</li>
-					<li>
-						<a>Info</a>
-					</li>
-					<li>
-						<a>About us</a>
-					</li>
-					<li>
-						<a>Contacts</a>
-					</li>
-					<li>
-						<a>Calendary</a>
-					</li>
-				</ul>
-			</div>
-		);
-	}
-}
+const pageMenu = 	<div>
+	<ul>
+		<li>
+			<a>Main</a>
+		</li>
+		<li>
+			<a>Info</a>
+		</li>
+		<li>
+			<a>About us</a>
+		</li>
+		<li>
+			<a>Contacts</a>
+		</li>
+		<li>
+			<a>Calendary</a>
+		</li>
+	</ul>
+</div>;
 
-export default Page;
+export default pageMenu;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Page from './Page/Page.js';
+import pageMenu from './Page/Page.js';
 
 ReactDOM.render(
-  <Page />,
-  document.getElementById('root')
+	pageMenu,
+	document.getElementById('root')
 );
