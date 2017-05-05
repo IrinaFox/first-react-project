fbf3378548f1ecdd0966ccf2551c163e67cf97ae
IrinaFox <irina.fox88@gmail.com>
2017-05-05 19:44:03 +0300

    add current time with life-cycle

--- src/Page/Page.js
+++ src/Page/Page.js
@@ -6,28 +6,73 @@ const username = {
 	lastName: 'Akunin'
 };
 
-class Page extends Component {
-	render() {
+function SayHello () {
+	return (
+		<div className="sayHello">
+			Hello, {username.firstName}!
+		</div>
+	);
+}
+
+function Menu () {
+	return (
+		<div>
+			<ul>
+				<li>
+					<a>Main</a>
+				</li>
+				<li>
+					<a>Info</a>
+				</li>
+				<li>
+					<a>About us</a>
+				</li>
+				<li>
+					<a>Contacts</a>
+				</li>
+				<li>
+					<a>Calendary</a>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+function CurrentTime (props){
+	return (
+		<h1>Time: {props.date.toLocaleTimeString()}</h1>
+	);
+}
+
+class Page extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+
+	componentDidMount () {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+
+	componentWillUnmount () {
+		clearInterval(this.timerID);
+	}
+
+	tick () {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render () {
 		return (
 			<div>
-				<ul>
-					<li>
-						<a>Main</a>
-					</li>
-					<li>
-						<a>Info</a>
-					</li>
-					<li>
-						<a>About us</a>
-					</li>
-					<li>
-						<a>Contacts</a>
-					</li>
-					<li>
-						<a>Calendary</a>
-					</li>
-				</ul>
-				<p>Hello, {username.firstName}!</p>
+				<CurrentTime date={this.state.date} />
+				<Menu />
+				<SayHello />
 			</div>
 		);
 	}
